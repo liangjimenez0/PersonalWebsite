827c232645a52a9fdb7ddec62b80b856a0386127
Liang jimenez <liangjimenez@yahoo.com>
2024-05-27 20:34:56 -0400

    navigation spelling

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,7 +6,7 @@ import { FiSun } from "react-icons/fi";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export function Naviation() {
+export function Navigation() {
   AOS.init();
 
   const links = [
