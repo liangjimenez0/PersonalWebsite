a630b9956b8523bfaed1b78646816a97a16a989a
Liang jimenez <liangjimenez@yahoo.com>
2024-07-29 21:23:11 +0800

    writing coming soon

--- src/pages/Writing/index.js
+++ src/pages/Writing/index.js
@@ -13,13 +13,18 @@ export function Writing() {
           </p>
         </div>
         <div className="articles flex flex-col align-left animate__animated animate__fadeIn">
+          <ArticleItem
+            description="Coming soon"
+            date="TBD"
+            time="0 minutes"
+          ></ArticleItem>
           {/* <ArticleItem
             title="6 months at Fortune"
             description="My first experience as an intern in NYC"
             date="May 2, 2024"
             time="17 minutes"
             href="https://liangjimenez.substack.com/"
-          ></ArticleItem>{" "} */}
+          ></ArticleItem> */}
         </div>
       </div>
     </div>
