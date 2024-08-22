35167d382f5560254979c5a1852fead0429f72e7
Liang jimenez <liangjimenez@yahoo.com>
2024-08-22 11:53:17 +0800

    remove play

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -11,7 +11,7 @@ export function Navigation() {
 
   const links = [
     { name: "Work", route: "work" },
-    { name: "Play", route: "play" },
+    // { name: "Play", route: "play" },
     // { name: "Writing", route: "writing" },
     { name: "About", route: "about" },
   ];
